b134addfe82fe0148d6ae8d6fd01a96920d14d95
kowalski <pksgmaster@gmail.com>
2025-08-12 12:12:43 +0700

    [FIX] alnas_xlsx: odoo binary webp image problem

--- alnas_xlsx/README.md
+++ alnas_xlsx/README.md
@@ -10,6 +10,9 @@ Before installing this module, make sure to install the following libraries:
 
 - `pip install xlsxtpl num2words Babel`
 
+Note: If you want to use replace image, please install webp
+- `sudo apt install webp`
+
 ## Usage
 
 For usage instructions, you can refer to the following video: [Link](https://youtu.be/-mpE5AaSJhw)  
@@ -17,6 +20,7 @@ For usage instructions, you can refer to the following video: [Link](https://you
 
 Documentation on xlsxtpl syntax in the document: [Link](https://pypi.org/project/xlsxtpl/)
 Example Template: [Link](https://github.com/alienyst/alnas-xlsx/raw/16.0/alnas_xlsx/static/description/example/example.xlsx)
+Example Template with Image: [Link](https://github.com/alienyst/alnas-xlsx/raw/16.0/alnas_xlsx/static/description/example/example_with_picture.xlsx)
 
 ## Field Naming Convention
 
@@ -27,6 +31,9 @@ To call and write the field name, use the following format: `{{docs.field_name}}
 - `{{spelled_out(docs.numeric_field)}}`: Spell out numbers
 - `{{formatdate(docs.date_field)}}`: Format dates
 - `{{convert_currency(docs.monetary_field, docs.currency_id)}}`: Format monetary field
+- `{% img docs.image_field %}`: Replace dummy image with image field, *require dummy image in template
+
+![Replace Image Syntax](assets/assets/replace_image.png)
 
 Note: The functions will be updated as needed.
 
--- alnas_xlsx/_monkeypatches/xlext.py
+++ alnas_xlsx/_monkeypatches/xlext.py
@@ -1,11 +1,17 @@
-from xltpl.xlext import ImageRef
-
-import base64
-import io
 import os
 import six
+import logging
+import subprocess
+import tempfile
+from io import BytesIO
+from base64 import b64decode
 from PIL import Image as PILImage, ImageFile
 
+from xltpl.xlext import ImageRef
+from odoo.tools.mimetypes import guess_mimetype
+
+
+_logger = logging.getLogger(__name__)
 
 def patch_init(self, image, image_index):
     self.image = image
@@ -17,14 +23,33 @@ def patch_init(self, image, image_index):
 
     if isinstance(image, bytes):
         try:
-            self.image = PILImage.open(io.BytesIO(base64.b64decode(image)))
-        except Exception:
+            imageb64 = b64decode(image)
+            # Handle webp images, convert to png
+            # i dont know why PIL cant read webp image from odoo binary field
+            if imageb64 and guess_mimetype(imageb64, '') == 'image/webp':
+                with tempfile.NamedTemporaryFile(delete=False, suffix='.webp') as temp_webp_file:
+                    temp_webp_file.write(imageb64)
+                    temp_webp_file_path = temp_webp_file.name
+
+                temp_png_file_path = temp_webp_file_path.replace('.webp', '.png')
+                subprocess.run(['dwebp', temp_webp_file_path, '-o', temp_png_file_path])
+
+                with open(temp_png_file_path, 'rb') as f:
+                    self.image = BytesIO(f.read())
+                    
+                os.remove(temp_webp_file_path)
+                os.remove(temp_png_file_path)
+                
+            else:
+                self.image = PILImage.open(BytesIO(imageb64))
+
+        except Exception as error:
+            _logger.error('Error processing image: %s', error)
             self.image = None
             
     elif not isinstance(image, ImageFile.ImageFile):
         fname = six.text_type(image)
         if not os.path.exists(fname):
             self.image = None
-
 
 ImageRef.__init__ = patch_init
