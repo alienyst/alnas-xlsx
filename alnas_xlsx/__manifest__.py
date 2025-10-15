1eb2cac9457653a6d04cfafefa3781368b1011a5
kowalski <pksgmaster@gmail.com>
2025-10-15 12:40:26 +0700

    [FIX] alnas_xlsx: fix damaged broken excel in odoo.sh

--- alnas_xlsx/__manifest__.py
+++ alnas_xlsx/__manifest__.py
@@ -9,7 +9,7 @@
     "website": "https://github.com/alienyst",
     "images": ["static/description/banner.png"],
     "category": "Technical",
-    "version": "15.0.1.0.2",
+    "version": "15.0.1.0.3",
     "application": True,
     "installable": True,
     "depends": ["base", "mail"],
