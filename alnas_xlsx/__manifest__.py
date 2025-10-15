4838fe805e7f1fbfab62c1c92f40360df7b6ad58
kowalski <pksgmaster@gmail.com>
2025-10-15 12:44:04 +0700

    [IMP] alnas-xlsx: add insert image tag

--- alnas_xlsx/__manifest__.py
+++ alnas_xlsx/__manifest__.py
@@ -1,30 +1,19 @@
 {
     'name': "XLSX Report Generator",
-
-    'summary': """
-        Generate your Report with Excel template""",
-
-    'description': """
-        Simple module to generate report with Excel template
-    """,
-
+    'summary': """Generate your Report with Excel template""",
+    'description': """Simple module to generate report with Excel template""",
     'author': "Ali Ns",
     'website': "https://github.com/alienyst",
     'images': ["static/description/banner.png"],
     'category': 'Technical',
-    'version': '1.1',
+    'version': '1.2',
     'application': True,
     'installable': True,
-
     'depends': ['base', 'mail'],
-
     'data': [
         'security/ir.model.access.csv',
-        
         'views/xlsx_report_config_view.xml',
-        
         'views/ir_action_report_view.xml',
-
         'views/webclient_templates.xml',
     ],
 
